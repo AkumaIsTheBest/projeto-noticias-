26bfbb167b65201b55ff84ded84195e59cc6acbd
Felipe Braiani <felipe.santos@ifms.edu.br>
2025-02-10 18:12:30 -0400

    Adicionado função. Ainda ajustando problemas com CORS

--- scripts/script.js
+++ scripts/script.js
@@ -18,4 +18,34 @@ async function loadEnv() {
     }
   }
 
-  loadEnv()
+  /**
+   * Função para chamar a API
+   * @param {string} endpoint 
+   * @param {Object} params 
+   * @param {Function} callback 
+   */
+function callApi(endpoint, params, callback) {
+  const url = 'https://newsapi.org/v2/';
+
+  const urlParams = new URLSearchParams(params).toString();
+  console.log(urlParams)
+
+  loadEnv().then((API_KEY) => {
+    fetch(`${url}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Access-control-allow-origin': '*',
+        'Authorization': `Bearer ${API_KEY}`
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      });
+  })
+}
+
+callApi('everything', { q: 'bitcoin', from: '2025-02-09', to: '2025-02-09' });
