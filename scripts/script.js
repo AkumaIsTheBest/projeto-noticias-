de588700296be92393a4f9cbe0ac8aa58aab338e
LuisCunha05 <luisf.cunha05@gmail.com>
2025-02-11 01:30:42 -0300

    script

--- scripts/script.js
+++ scripts/script.js
@@ -18,4 +18,39 @@ async function loadEnv() {
     }
   }
 
-  loadEnv()
+
+
+/**
+ * @typedef {'everything' | 'top-headlines'} endPoints
+ */
+const DEV = true
+const API_URL = 'https://newsapi.org/v2/'
+const API_KEY = loadEnv()
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const url = `${proxyUrl}https://newsapi.org/v2/everything?apiKey=${API_KEY}`;
+const request = new Request(url);
+/**
+ *
+ * @param {endPoints} path
+ * @returns {Promise<{message: string}|any>}
+ */
+const useApi = async (path) => {
+    DEV && console.log(API_URL + path);
+    const header = {Authorization: API_KEY}
+
+    try {
+        const response = await fetch(`${request}`);
+        const jsonResponse = await response.json();
+
+        if (response.ok) {
+            return jsonResponse;
+        } else {
+            throw new Error(jsonResponse.message || 'Something went wrong');
+        }
+    } catch (err) {
+        console.error(err);
+        return { message:'Não foi possível se conectar com o servidor, tente novamente' }
+    }
+};
+
+console.log(useApi());
